795be309787a58adb0aa11d3f8dd38fb4fcbb36b
rhkr8521 <rhkr8521@gmail.com>
2025-07-15 15:26:53 +0900

    [FIX] webClient 버퍼 크기 조정

--- src/main/java/com/flipflick/backend/common/config/webclient/WebClientConfig.java
+++ src/main/java/com/flipflick/backend/common/config/webclient/WebClientConfig.java
@@ -3,18 +3,26 @@ package com.flipflick.backend.common.config.webclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    @Value("${tmdb.api.url}")
-    private String apiUrl;
-
     @Bean
-    public WebClient tmdbWebClient(WebClient.Builder builder) {
+    public WebClient tmdbWebClient(WebClient.Builder builder,
+                                   @Value("${tmdb.api.url}") String baseUrl) {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer
+                        .defaultCodecs()
+                        // 10 * 1024 * 1024 = 10MB
+                        .maxInMemorySize(10 * 1024 * 1024)
+                )
+                .build();
+
         return builder
-                .baseUrl(apiUrl)
+                .baseUrl(baseUrl)
+                .exchangeStrategies(strategies)
                 .build();
     }
 }
