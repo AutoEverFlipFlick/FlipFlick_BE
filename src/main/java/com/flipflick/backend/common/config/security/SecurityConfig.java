cebe58dde749d3fccf5b47461a7766e1a0f3e045
Taegeun Kwack <rhkr8521@gmail.com>
2025-07-16 17:50:25 +0900

    [FIX] 헬스체크 인증 허가

--- src/main/java/com/flipflick/backend/common/config/security/SecurityConfig.java
+++ src/main/java/com/flipflick/backend/common/config/security/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/member/login", "/api/v1/member/signup", "/api/v1/member/reissue", "/v3/api-docs/**",
-                                "/swagger-ui/**", "/swagger-resources/**", "/webjars/**", "/h2-console/**").permitAll() // 회원, 스웨거, H2 인증 허가
+                                "/swagger-ui/**", "/swagger-resources/**", "/webjars/**", "/h2-console/**", "/health").permitAll() // 회원, 스웨거, H2 인증 허가
                         .requestMatchers("/admin").hasRole("ADMIN") // 관리자 페이지 Role 체크
                         .requestMatchers("/api/v1/search/movie", "/api/v1/search/cast").permitAll() // 검색 인증 허가
                         .requestMatchers("/api/v1/movie/view", "/api/v1/cast/view").permitAll() // 영화, 배우 상세 조회 인증 허가
