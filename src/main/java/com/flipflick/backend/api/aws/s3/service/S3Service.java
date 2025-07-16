a9f623263051fb91b9c4b30fc6405d8766c5538c
taemin Kim <ruhokim1@naver.com>
2025-07-17 02:23:11 +0900

    [FEAT] AWS S3 이미지 업로드 기능 구현

---
+++ src/main/java/com/flipflick/backend/api/aws/s3/service/S3Service.java
@@ -0,0 +1,63 @@
+package com.flipflick.backend.api.aws.s3.service;
+
+import com.flipflick.backend.common.exception.BadRequestException;
+import com.flipflick.backend.common.response.ErrorStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile file, String dirName) throws IOException {
+        if (file == null || file.isEmpty()) {
+            throw new BadRequestException(ErrorStatus.EMPTY_FILE.getMessage());
+        }
+        String fileName = dirName + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileName)
+                .acl(ObjectCannedACL.PUBLIC_READ)  // 공개 URL 접근
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return getFileUrl(fileName);
+    }
+
+    public String getFileUrl(String fileName) {
+        return "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" + fileName;
+    }
+
+    public void deleteImage(String imageUrl) {
+        String baseUrl = "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/";
+        if (imageUrl != null && imageUrl.startsWith(baseUrl)) {
+            String key = imageUrl.substring(baseUrl.length());
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+        }
+
+
+    }
+}
